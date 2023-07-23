f792fb39eb4e3268a30e871b28802f0bcac0fc07
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-22 20:42:01 -0500

    Made header component. Added logic for entering bill amount, calculating the tip, and also calculating the total.

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Tip Calculator</h1>
+        <div>
+          <p>
+            Please rate the service of your meal between 1 and 10. 1 being
+            "Horrible" and 10 being "Perfect"! Please remember the quality of
+            your food was not the server's fault!
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import Header from './Header';
 
 const TipCalculator = () => {
-return <h1>This is going to be a tip calculator!</h1>
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+
+    const handleBillChange = (event) => {
+    setBill(Number(event.target.value));
+  };
+
+  const calculateTip = () => {
+
+    const service = Number(
+      document.querySelector('input[name="service"]:checked').value
+    );
+
+    let X;
+    if (service <= 3) {
+      X = 0.15;
+    } else if (service >= 4 && service <= 5) {
+      X = 0.18;
+    } else if (service >= 6 && service <= 7) {
+      X = 0.21;
+    } else if (service === 8) {
+      X = 0.25;
+    } else if (service >= 9 && service <= 10) {
+      X = 0.3;
+    }
+
+    const tipAmount = Number((bill * X).toFixed(2));
+    setTip(tipAmount);
+
+  };
+
+  const calculateTotal = () => {
+
+    const total = bill + tip;
+    return total;
+
+  };
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+
 }
 
 export default TipCalculator;
